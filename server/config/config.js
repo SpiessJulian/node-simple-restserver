675af32cb471b28d7baf426e6a0b827f1fc20282
Julian <spiessapps@gmail.com>
2020-12-02 03:08:27 -0300

    Error in db connect fix

--- server/config/config.js
+++ server/config/config.js
@@ -15,9 +15,10 @@ process.env.ENV = process.env.ENV || 'dev';
 //==============================
 let urlDB;
 
-if (process.env.ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = 'mongodb+srv://julianmongo:tOQlL0M1kp8YUvNq@cluster0.vxytg.mongodb.net/cafe';
 }
+
 process.env.URLDB = urlDB;
